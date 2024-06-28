00f85ecbbc565eb7e9020864e81e60983d82cd32
Guw2 <gustafar79@gmail.com>
2024-06-28 17:13:00 -0300

    Start Point : Páginas de Login e Welcome e Servlet para Validação dos Dados (Estáticos Por Enquanto)

---
+++ src/main/java/login/Login.java
@@ -0,0 +1,25 @@
+package login;
+
+import java.io.IOException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equals("admin") && password.equals("admin")) {
+			response.sendRedirect("welcome.jsp");
+		}else {
+			response.sendRedirect("login.jsp");
+		}
+	}
+	
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body style="font-family: arial; zoom: 2; text-align: center;">
+	
+	<h1>Log-in Page</h1>
+	
+	<hr style="margin-bottom: 20px;">
+	
+	<form action="Login">
+		
+		USERNAME : <input type="text" name="username"><br>
+		PASSWORD : <input type="password" name="password" style="margin-top: 5px;"><br>
+		
+		<input type="submit" value="login" style="margin-top: 15px;">
+	</form>
+	
+</body>
+</html>
